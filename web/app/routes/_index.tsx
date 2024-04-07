7033ff454c47e90ff215dfa5f102d2d245daf2b3
Ramana Venkata <vramana@users.noreply.github.com>
2024-04-07 20:37:35 +0530

    Fetch workflows and workflow runs

--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -1,40 +1,50 @@
-import type { MetaFunction } from "@remix-run/node";
+import { json, type MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { Octokit } from "@octokit/rest";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Watch My CI" },
+    { name: "description", content: "Get insights into CI workflows" },
   ];
 };
 
+export const loader = async () => {
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+  const user = await octokit.rest.users.getByUsername({ username: "vramana" });
+
+  const runs = await octokit.rest.actions.listWorkflowRunsForRepo({
+    owner: "vramana",
+    repo: "watch-my-ci",
+  });
+
+  const workflows = await octokit.rest.actions.listRepoWorkflows({
+    owner: "vramana",
+    repo: "watch-my-ci",
+  });
+  return json({ user: user.data, runs: runs.data, workflows: workflows.data });
+};
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Watch My CI</h1>
+      <p>{data.user.name}</p>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {data.runs.workflow_runs.map((run) => (
+          <li key={run.id}>
+            {run.name} - {run.status} - {run.head_branch} - {run.workflow_id}
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {data.workflows.workflows.map((workflow) => (
+          <li key={workflow.id}>
+            {workflow.id} - {workflow.name} - {workflow.path}
+          </li>
+        ))}
       </ul>
     </div>
   );
