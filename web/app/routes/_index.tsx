4a60f5a6f5d01903f7d75f050fae48afaa62b269
Sarvani Sanaboyina <sarvani.sanaboyina@gmail.com>
2024-05-21 16:59:24 +0530

    clean up and add form

--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -1,6 +1,6 @@
 import { json, type MetaFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { Octokit } from "@octokit/rest";
+import { Form } from "@remix-run/react";
+import { useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,60 +9,23 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export const loader = async () => {
-  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-  const user = await octokit.rest.users.getByUsername({ username: "vramana" });
-
-  const runs = await octokit.rest.actions.listWorkflowRunsForRepo({
-    owner: "vramana",
-    repo: "watch-my-ci",
-  });
-
-  const workflows = await octokit.rest.actions.listRepoWorkflows({
-    owner: "nodejs",
-    repo: "node",
-  });
-
-  octokit
-    .paginate(octokit.rest.actions.listWorkflowRunsForRepo, {
-      owner: "vramana",
-      repo: "watch-my-ci",
-    })
-    .then((issues) => {
-      console.log("issues------>", issues);
-      // issues is an array of all issue objects
-    });
-
-  const jobs = await octokit.rest.actions.listJobsForWorkflowRun({
-    owner: "vramana",
-    repo: "watch-my-ci",
-    run_id: runs.data.workflow_runs[0].id,
-  });
-
-  return json({ user: user.data, runs: runs.data, workflows: workflows.data });
-};
-
 export default function Index() {
-  const data = useLoaderData<typeof loader>();
+  const [repo, setRepo] = useState("");
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Watch My CI</h1>
-      <p>{data.user.name}</p>
-      <ul>
-        {data.runs.workflow_runs.map((run) => (
-          <li key={run.id}>
-            {run.name} - {run.status} - {run.head_branch} - {run.workflow_id}
-          </li>
-        ))}
-      </ul>
-      <ul>
-        {data.workflows.workflows.map((workflow) => (
-          <li key={workflow.id}>
-            {workflow.id} - {workflow.name} - {workflow.path}
-          </li>
-        ))}
-      </ul>
+
+      <Form action={`/public/${repo}`}>
+        <input
+          type="text"
+          placeholder="Repo"
+          value={repo}
+          onChange={(e) => setRepo(e.target.value)}
+        />
+        <p>Example: nodejs/nodejs</p>
+        <button type="submit">Go</button>
+      </Form>
     </div>
   );
 }
