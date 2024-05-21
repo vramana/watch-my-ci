00b65512f30348c8223008a23093cfa3ee6926e2
Sarvani Sanaboyina <sarvani.sanaboyina@gmail.com>
2024-05-21 18:27:24 +0530

    implement navBar

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -5,6 +5,33 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import stylesheet from "~/tailwind.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
+
+function Nav() {
+  return (
+    <nav className="bg-gray-900 border-gray-200 ">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-3">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
+            Watch my CI
+          </span>
+        </a>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+          <button className="m-0 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-gray-800">
+            <a href="/" aria-current="page">
+              Login
+            </a>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -15,8 +42,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
-        {children}
+      <body className="bg-gray-100">
+        <Nav />
+        <div className=" container mx-auto px-4">{children}</div>
         <ScrollRestoration />
         <Scripts />
       </body>
