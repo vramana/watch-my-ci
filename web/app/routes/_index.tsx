958d7cc86802fb400d5859280e61c316f79474fe
Sarvani Sanaboyina <sarvani.sanaboyina@gmail.com>
2024-05-21 18:27:56 +0530

    style index page

--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -13,18 +13,24 @@ export default function Index() {
   const [repo, setRepo] = useState("");
 
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Watch My CI</h1>
-
+    <div className="text-center py-10">
+      <h4 className="text-gray-900 py-6 font-black text-5xl">
+        Enter your GIT repo to watch and track CI
+      </h4>
       <Form action={`/public/${repo}`}>
         <input
           type="text"
-          placeholder="Repo"
+          placeholder="fastify/fastify"
+          className="p-2 rounded-md"
           value={repo}
           onChange={(e) => setRepo(e.target.value)}
         />
-        <p>Example: nodejs/nodejs</p>
-        <button type="submit">Go</button>
+        <button
+          className="mx-4 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+          type="submit"
+        >
+          Go
+        </button>
       </Form>
     </div>
   );
