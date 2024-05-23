a7d16b9957af7c584b8a463ac800d6f8a4cfdbfa
Sarvani Sanaboyina <sarvani.sanaboyina@gmail.com>
2024-05-23 23:59:28 +0530

    fix theme css

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -22,7 +22,7 @@ function Nav() {
           </span>
         </a>
         <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-          <button className="m-0 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-gray-800">
+          <button className="m-0 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5">
             <a href="/" aria-current="page">
               Login
             </a>
